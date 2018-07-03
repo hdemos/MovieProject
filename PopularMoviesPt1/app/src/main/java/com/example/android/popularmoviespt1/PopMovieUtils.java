260ab34b7669988dc1e566bd0456d0b489d70d57
Haley Demos <haleydemosother@gmail.com>
2018-07-03 18:19:25 -0500

    Changing to manual JSON parsing

--- PopularMoviesPt1/app/src/main/java/com/example/android/popularmoviespt1/PopMovieUtils.java
+++ PopularMoviesPt1/app/src/main/java/com/example/android/popularmoviespt1/PopMovieUtils.java
@@ -8,10 +8,15 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -19,6 +24,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class PopMovieUtils {
+    private static final String TAG = PopMovieUtils.class.getSimpleName();
 
     final static String API_KEY = "69e9f8e34637b9e6f6f8e56ce742d1dc"; //set to null when submitting
     final static String MOVIE_BASE_URL = "https://api.themoviedb.org/3/movie/";
@@ -40,6 +46,32 @@ public class PopMovieUtils {
         }
         return retrofit.create(movieClass);
     }
+
+    public static void parseJson(String data, ArrayList<Movie> list) {
+        JSONObject mainObject = new JSONObject(data);
+//TODO 10: change for my getters and setters
+        JSONArray resArray = mainObject.getJSONArray("results"); //Getting the results object
+        for (int i = 0; i < resArray.length(); i++) {
+            JSONObject jsonObject = resArray.getJSONObject(i);
+            Movie movie = new Movie(); //New Movie object
+            movie.setId(jsonObject.getInt("id"));
+            movie.setVoteAverage(jsonObject.getInt("vote_average"));
+            movie.setVoteCount(jsonObject.getInt("vote_count"));
+            movie.setOriginalTitle(jsonObject.getString("original_title"));
+            movie.setTitle(jsonObject.getString("title"));
+            movie.setPopularity(jsonObject.getDouble("popularity"));
+            movie.setBackdropPath(jsonObject.getString("backdrop_path"));
+            movie.setOverview(jsonObject.getString("overview"));
+            movie.setReleaseDate(jsonObject.getString("release_date"));
+            movie.setPosterPath(jsonObject.getString("poster_path"));
+            //Adding a new movie object into ArrayList
+            list.add(movie);
+        }
+    } catch (JSONException e) {
+        e.printStackTrace();
+        Log.e(TAG, "Erro occurred during JSON Parsing", e);
+    }
+}
 //
 //    final static String PARAM_QUERY = "?";
 //    final static String PARAM_SORT = "sort_by";
